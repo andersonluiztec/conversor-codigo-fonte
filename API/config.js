65c4280bc252c50bf30d62e8dfbbf1e350e41e8b
Anderson Silva <andersonluiztec@gmail.com>
2023-05-24 13:56:44 -0300

    Alteração Arquivo Configuração

--- API/config.js
+++ API/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apiUrl: 'https://api.openai.com/v1/completions',
-    apiKey: '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',
+    apiKey: '{key}',
     port: 3000,
     baseURL: 'http://localhost'
   };
